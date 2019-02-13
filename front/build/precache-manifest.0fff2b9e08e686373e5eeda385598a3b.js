ee44573f3bbb53c90aca6224fc343db5e3f71183
Andrei Sokolov <xlloll3@gmail.com>
2019-02-13 15:39:34 +0300

    builded prod

---
+++ front/build/precache-manifest.0fff2b9e08e686373e5eeda385598a3b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ac6624350ec0fd7c16cd",
+    "url": "/static/js/main.ac662435.chunk.js"
+  },
+  {
+    "revision": "c5ab31d7557d03fe208f",
+    "url": "/static/js/2.c5ab31d7.chunk.js"
+  },
+  {
+    "revision": "430b4e43856df4483f3ddc8aceefeb75",
+    "url": "/index.html"
+  }
+];
